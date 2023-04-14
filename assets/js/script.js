7fd730147c373b90f21d5fec5c25de43be32e03c
bswiley <35941707+bswiley@users.noreply.github.com>
2023-04-14 17:10:08 -0500

    Made it possible to pull from localStorage and not repeat cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,68 @@
-var date = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
+var date  = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
 var image = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
-var temp = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
-var wind = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
-var humidity = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
-var submit = $(".searchbtn");
+var temp  = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
+var wind  = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
+var humid = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
+var date2 =   [0,1,2,3,4]
+var temp2 =   [0,1,2,3,4]
+var icon2=   [0,1,2,3,4]
+var wind2 =   [0,1,2,3,4]
+var humid2=   [0,1,2,3,4]
+var submit      = $(".searchbtn");
 var formContent = $("#searchfrm");
-var buttonList = $("#buttonList");
-var cityList =["safeWord"]
-var bHumidity = $("bHumidity");
-var bWind = $("bWind");
-var bTemp = $("bTemp");
-var cityDate = $("#cityDate");
-var bImage = $("#bImage");
-var searchfrm = $("#searchfrm");
-var searchbtn = $("#searchbtn");
-var cCity = $("#currentCity");
-var cList = $("#oldCityList");
-var oCity1 = $("#oldCity1");
-var oCity2 = $("#oldCity2");
-var oCity3 = $("#oldCity3");
-var oCity4 = $("#oldCity4");
-var oCity5 = $("#oldCity5");
-var oCity6 = $("#oldCity6");
-var oCity7 = $("#oldCity7");
-var oCity8 = $("#oldCity8");
-var oCity9 = $("#oldCity9");
-var oCity10 = $("#oldCity10");
+var buttonList  = $("#buttonList");
+var cityList    = ["safeWord"]
+var bHumid      = $("bHumid");
+var bWind       = $("bWind");
+var bTemp       = $("bTemp");
+var cityDate    = $("#cityDate");
+var bImage      = $("#bImage");
+var searchfrm   = $("#searchfrm");
+var searchbtn   = $("#searchbtn");
+var cCity       = $("#currentCity");
+var cList       = $("#oldCityList");
+var oCity1      = $("#oldCity1");
+var oCity2      = $("#oldCity2");
+var oCity3      = $("#oldCity3");
+var oCity4      = $("#oldCity4");
+var oCity5      = $("#oldCity5");
+var oCity6      = $("#oldCity6");
+var oCity7      = $("#oldCity7");
+var oCity8      = $("#oldCity8");
+var oCity9      = $("#oldCity9");
+var oCity10     = $("#oldCity10");
 var card1 = $("card1");
 var card2 = $("card2");
 var card3 = $("card3");
 var card4 = $("card4");
 var card5 = $("card5");
- date[5] = $("#date0");
- image[5] =$("#image0");
- temp[5] = $("#temp0");
- wind[5] =$("#wind0");
-humidity[5] = $("#humidity0");
-date[13] =$("#date1");
-image[13] =$("#image1");
-temp[13] = $("#temp1");
-wind[13] =$("#wind1");
-humidity[13] = $("#humidity1");
-date[21] =$("#date2");
-image[21]=$("#image2");
-temp[21] = $("#temp2");
-wind[21] =$("#wind2");
-humidity[21] = $("#humidity2");
-date[29] =$("#date3");
-image[29] =$("#image3");
-temp[29] = $("#temp3");
-wind[29] =$("#wind3");
-humidity[29] = $("#humidity3");
-date[37] =$("#date4");
-image[37] =$("#image4");
-temp[37] = $("#temp4");
-wind[37] =$("#wind4");
-humidity[37] = $("#humidity4");
+date[5]      = date2[0]      = $("#date0");
+image[5]     = icon2[0]      = $("#image0");
+temp[5]      = temp2[0]      = $("#temp0");
+wind[5]      = wind2[0]      = $("#wind0");
+humid[5]     = humid2[0]     = $("#humid0");
+date[13]     = date2[1]      = $("#date1");
+image[13]    = icon2[1]      = $("#image1");
+temp[13]     = temp2[1]      = $("#temp1");
+wind[13]     = wind2[1]      = $("#wind1");
+humid[13]    = humid2[1]     = $("#humid1");
+date[21]     = date2[2]      = $("#date2");
+image[21]    = icon2[2]      = $("#image2");
+temp[21]     = temp2[2]      = $("#temp2");
+wind[21]     = wind2[2]      = $("#wind2");
+humid[21]    = humid2[2]     = $("#humid2");
+date[29]     = date2[3]      = $("#date3");
+image[29]    = icon2[3]      = $("#image3");
+temp[29]     = temp2[3]      = $("#temp3");
+wind[29]     = wind2[3]      = $("#wind3");
+humid[29]    = humid2[3]     = $("#humid3");
+date[37]     = date2[4]      = $("#date4");
+image[37]    = icon2[4]      = $("#image4");
+temp[37]     = temp2[4]      = $("#temp4");
+wind[37]     = wind2[4]      = $("#wind4");
+humid[37]    = humid2[4]     = $("#humid4");
+
+
 
 var today = dayjs().format('YYYYMMDD');
 
@@ -73,11 +80,10 @@ if (localStorage.getItem(today)===null){
   var storage = false
 } 
 else {
-  var storage = true
+  var storage = true}
   var storedCities = JSON.parse(localStorage.getItem(today))
   var storedCitiesArray = storedCities["Cities"]
   var storedCitiesNum = storedCitiesArray.length;
-
 console.log(storedCitiesNum)
   for (var i=0 ; i<4 ; i++){
     city = storedCitiesArray[i]
@@ -109,7 +115,7 @@ var makeCityButton = function(city){
 
 buttonList.click(function(event){
 var activeOldCity = event.target.className
-getForecast(activeOldCity);
+//getWeather(activeOldCity);
 loadData(activeOldCity);
 });
 
@@ -118,9 +124,33 @@ function loadData(city){
 console.log(city)
 var data =   JSON.parse(localStorage.getItem(city))
 console.log (data);
-for (var i = 0;i<4 ;i++ ){
-  pass
-}}
+data = data.city;
+console.log (data);
+for (var i = 0;i<5 ;i++ ){
+ var weather=data[i];
+ date2[i].text(weather[0]);
+ icon2[i].attr("src",weather[1]);
+ temp2[i].text(weather[2]);
+ wind2[i].text(weather[3]);
+ humid2[i].text(weather[4]);
+ console.log (weather);
+ console.log (i);
+ console.log (date);
+ console.log (weather[0]);
+ console.log (weather[1]);
+ console.log (weather[2]);
+ console.log (weather[3]);
+ console.log (weather[4]);
+ getWeather(city);
+
+};}
+
+submit.on("click",handleSubmit);
+
+
+
+
+
 
 async function getWeather(city){
   queryUrl ='https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=c678bf4ba5b2185e3326c14c3b82bf12'
@@ -129,7 +159,7 @@ async function getWeather(city){
       queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=c678bf4ba5b2185e3326c14c3b82bf12'
     }
 
-    fetch(queryUrl,date,temp,wind,humidity)
+    fetch(queryUrl)
       .then(function (response) {
         if (!response.ok) {
           throw response.json();
@@ -168,7 +198,7 @@ console.log(forStorage);
 var weatherhumid = weatherData.main.humidity
 var displayHumid = weatherhumid +"%"
 console.log (displayHumid) 
-this.bHumidity.text(displayHumid)
+this.bHumid.text(displayHumid)
 forStorage[4]=displayHumid;   
 console.log(forStorage)
 saveStorage = {
@@ -178,8 +208,6 @@ console.log(saveStorage);
 localStorage.setItem((city+'0'),JSON.stringify(saveStorage));
 console.log(saveStorage);
 })}; 
-
-
 
 async function getForecast(city){
 
@@ -233,21 +261,18 @@ this.wind[i].text(displayWind);
 var weatherhumid = weatherData.list[i].main.humidity
 var displayHumid = weatherhumid +"%"
 console.log (displayHumid) 
-humidity[i].text(displayHumid)
+humid[i].text(displayHumid)
 forStorage[i+4]=displayHumid;
 }      
 console.log(forStorage)
 saveStorage = {
-  "Day0": [forStorage[5],forStorage[6],forStorage[7],forStorage[8],forStorage[9]],
-  "Day1": [forStorage[13],forStorage[14],forStorage[15],forStorage[16],forStorage[17]],
-  "Day2": [forStorage[21],forStorage[22],forStorage[23],forStorage[24],forStorage[25]],
-  "Day3": [forStorage[29],forStorage[30],forStorage[31],forStorage[32],forStorage[33]],
-  "Day4": [forStorage[37],forStorage[38],forStorage[39],forStorage[40],forStorage[41]]
- };
+  city: [[forStorage[5],forStorage[6],forStorage[7],forStorage[8],forStorage[9]],
+   [forStorage[13],forStorage[14],forStorage[15],forStorage[16],forStorage[17]],
+   [forStorage[21],forStorage[22],forStorage[23],forStorage[24],forStorage[25]],
+   [forStorage[29],forStorage[30],forStorage[31],forStorage[32],forStorage[33]],
+   [forStorage[37],forStorage[38],forStorage[39],forStorage[40],forStorage[41]]
+ ]};
 console.log(saveStorage);
 localStorage.setItem((city),JSON.stringify(saveStorage));
 console.log(saveStorage);
 })};
-
-
-submit.on("click",handleSubmit);
